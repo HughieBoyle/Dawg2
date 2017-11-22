5ec641ac5b1e0fce00e3ef3a9246cd1a8f491d9d
Kieran Thompson <l00118651@student.lyit.ie>
2017-11-22 12:23:53 +0000

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+
 
 import { AppComponent } from './app.component';
 import { DetailsFormComponent } from './details-form/details-form.component';
+import { DetailsTableComponent } from './details-table/details-table.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailsFormComponent
+    DetailsFormComponent,
+    DetailsTableComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
