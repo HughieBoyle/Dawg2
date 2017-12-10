6b8acc28610aca6b6e5ac97a8328740e3d315bf7
HughieBoyle <l00130487@student.lyit.ie>
2017-12-10 20:04:43 +0000

    New version, push directly from Eclipse

---
+++ health/src/main/java/Healthcare2/health/MongoConnection.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package Healthcare2.health;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+
+import static com.mongodb.client.model.Filters.*;
+/**
+ * @author BoyleHugh(L00130487)
+ *
+ */
+public class MongoConnection 
+{
+
+	/**
+	 * @param args
+	 */
+	double quote = 0;
+	public static void main(String[] args) 
+	{
+		MongoClient mC = new MongoClient("localhost", 27017);
+
+		String cP = mC.getConnectPoint();
+		System.out.println(cP);
+		MongoDatabase database = mC.getDatabase("test1");
+		for (String name : database.listCollectionNames())
+		{
+			System.out.println(name);
+		}
+		MongoCollection<Document> coll = database.getCollection("collection1");
+		
+		/*DeleteResult deleteResult = coll.deleteMany(gte("FName", "Hughie"));
+		System.out.println(deleteResult.getDeletedCount());*/
+		
+		MongoCursor<Document> cursor = coll.find().iterator();
+		try {
+		    while (cursor.hasNext()) 
+		    {
+		        System.out.println(cursor.next().toJson());
+		    }
+		} finally 
+		{
+		    cursor.close();
+		}
+
+		mC.close();
+	}
+	
+}
