978fe8e71ff5e97194a8dfcb1103450b682ea478
HughieBoyle <33486045+HughieBoyle@users.noreply.github.com>
2017-12-15 16:44:46 +0000

    Add files via upload

--- MongoConnection.java
+++ MongoConnection.java
@@ -1,58 +1,105 @@
 /**
  * 
  */
-package insuranceProject;
+package Healthcare2.health;
 
+import java.util.ArrayList;
 import org.bson.Document;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.Block;
+import org.bson.conversions.Bson;
+import static com.mongodb.client.model.Filters.*;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.result.DeleteResult;
-
-import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
 /**
  * @author BoyleHugh(L00130487)
  *
  */
-public class MongoConnection 
+public class MongoConnection  
 {
-
 	/**
 	 * @param args
 	 */
-	double quote = 0;
+	static double quote = 0;
 	public static void main(String[] args) 
-	{
+	{		
 		MongoClient mC = new MongoClient("localhost", 27017);
-
+		/*get and print the port number*/
 		String cP = mC.getConnectPoint();
 		System.out.println(cP);
-		MongoDatabase database = mC.getDatabase("test1");
+		/*connect to the "test" database
+		 */	MongoDatabase database = mC.getDatabase("test");
+		/*list all the collections in the database*/
 		for (String name : database.listCollectionNames())
 		{
 			System.out.println(name);
 		}
-		MongoCollection<Document> coll = database.getCollection("collection1");
-		
-		DeleteResult deleteResult = coll.deleteMany(gte("FName", "Hughie"));
-		System.out.println(deleteResult.getDeletedCount());
-		
+		/*connect to the collection "DawgHealth"*/
+		MongoCollection<Document> coll = database.getCollection("DawgHealth");
+		/*print the collection*/
 		MongoCursor<Document> cursor = coll.find().iterator();
 		try {
 		    while (cursor.hasNext()) 
 		    {
-		        System.out.println(cursor.next().toJson());
+		        System.out.println(cursor.next());
 		    }
-		} finally 
+		}
+		finally 
 		{
 		    cursor.close();
 		}
-
 		mC.close();
+		getQuote();
 	}
-	
+	public static void setQuote(Boolean occupationIn, Boolean conditionIn)
+	{
+		Boolean occupation = occupationIn;
+		Boolean condition = conditionIn;
+		/*update quote if occupation is selected*/
+		if(occupation == true)
+		{
+			quote = quote + quote/10;
+		}
+		/*update quote if condition is selected*/
+		if(condition == true)
+		{
+			quote = quote + quote/10;
+		}
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoDatabase database = mongoClient.getDatabase("test");
+        MongoCollection<Document> collection = database.getCollection("DawgHealth");
+        
+        /*filter the collection by name*/
+        Bson filter = new Document("FName", "Hughie");
+        /*set the new value*/
+        Bson newQuote = new Document("Quote", 2.0);
+        /*update the document*/
+        Bson updateDocument = new Document("$set", newQuote);
+        collection.updateOne(filter, updateDocument);
+        mongoClient.close();
+	}
+	public static String getQuote() 
+	{
+		String sQ = null;
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoDatabase database = mongoClient.getDatabase("test");
+        MongoCollection<Document> collection = database.getCollection("DawgHealth");
+        
+        /*filter by name and only show the field "Quote"*/
+        FindIterable<Document> it = collection.find(eq("FName", "Hughie"))
+        		.projection(fields(include("Quote"),excludeId()));
+        ArrayList<Document> docs = new ArrayList<Document>();       
+        it.into(docs);
+        /*set value to String variable*/
+       for (Document doc : docs) 
+        {
+        	sQ = doc.toString();
+/*        	print for testing
+            System.out.println(sQ);*/
+        }     
+        mongoClient.close();
+        return sQ;
+	}
 }
